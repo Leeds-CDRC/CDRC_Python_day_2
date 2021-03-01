c4fe1c58fa45fd87de3636965e85898c96217e64
Francesca Pontin <32068761+FrancescaPontin@users.noreply.github.com>
2021-03-01 07:38:08 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+setup(
+    name='geoplot',
+    packages=['geoplot'],
+    install_requires=[
+        'matplotlib', 'seaborn', 'pandas', 'geopandas', 'cartopy', 'descartes',
+        'mapclassify>=2.1', 'contextily>=1.0.0'
+    ],
+    extras_require={'develop': [
+        'pytest', 'pytest-mpl', 'scipy', 'pylint', 'jupyter', 'sphinx', 'sphinx-gallery',
+        'sphinx_rtd_theme', 'mplleaflet'
+    ]},
+    py_modules=['geoplot', 'crs', 'utils', 'ops'],
+    version='0.4.1',
+    python_requires='>=3.6.0',
+    description='High-level geospatial plotting for Python.',
+    author='Aleksey Bilogur',
+    author_email='aleksey.bilogur@gmail.com',
+    url='https://github.com/ResidentMario/geoplot',
+    download_url='https://github.com/ResidentMario/geoplot/tarball/0.4.1',
+    keywords=[
+        'data', 'data visualization', 'data analysis', 'data science', 'pandas', 'geospatial data',
+        'geospatial analytics'
+    ],
+    classifiers=['Framework :: Matplotlib'],
+)
